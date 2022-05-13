053a6d81cd1225ff9376ca92fc39e9af8c4389ce
hamaju <jushii@gmail.com>
2022-05-13 20:49:10 +0300

    section 18

--- section18/deploy/src/main.js
+++ section18/deploy/src/main.js
@@ -1,9 +1,8 @@
 import { createApp } from 'vue';
 
+import router from './router.js';
+import store from './store/index.js';
 import App from './App.vue';
-import router from './router';
-import store from './store/index';
-
 import BaseCard from './components/ui/BaseCard.vue';
 import BaseButton from './components/ui/BaseButton.vue';
 import BaseBadge from './components/ui/BaseBadge.vue';
